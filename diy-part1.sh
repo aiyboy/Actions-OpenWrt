1d11ebff109ee7e3b39adc6047b34e2513a4a568
aiyboy <59334928+aiyboy@users.noreply.github.com>
2021-05-23 10:20:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,12 +9,19 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+#
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+#
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+#
 # Lienol/openwrt-package
 sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+#
+# kenzok8/openwrt-packages
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+#
+# kenzok8/small
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+
