d18acbb8432cd2e9408a824cd873423a3c76b420
aiyboy <59334928+aiyboy@users.noreply.github.com>
2021-05-23 16:59:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,15 +13,5 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #
-# Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#
-# Lienol/openwrt-package
-sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-packag' feeds.conf.default
-#
-# kenzok8/openwrt-packages
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#
-# kenzok8/small
-sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+
 
